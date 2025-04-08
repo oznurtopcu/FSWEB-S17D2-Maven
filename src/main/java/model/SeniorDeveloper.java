6c24d5f39dff9ea4c6ed582e022de8c056091e46
oznurtopcu <topcuoznur121@gmail.com>
2025-04-08 14:22:49 +0300

    create senior developer

---
+++ src/main/java/model/SeniorDeveloper.java
@@ -0,0 +1,9 @@
+package model;
+
+public class SeniorDeveloper extends Developer{
+    public SeniorDeveloper(int id, String name, double salary) {
+        super(id, name, salary, Experience.SENIOR);
+    }
+
+
+}
