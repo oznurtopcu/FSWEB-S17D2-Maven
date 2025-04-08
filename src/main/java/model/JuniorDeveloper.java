cc3fcdd37d984ca8555954de2339bbf15bfa52d9
oznurtopcu <topcuoznur121@gmail.com>
2025-04-08 14:11:58 +0300

    create junior developer class

---
+++ src/main/java/model/JuniorDeveloper.java
@@ -0,0 +1,7 @@
+package model;
+
+public class JuniorDeveloper extends Developer{
+    public JuniorDeveloper(long id, String name, double salary, Experience experience) {
+        super(id, name, salary, experience);
+    }
+}
