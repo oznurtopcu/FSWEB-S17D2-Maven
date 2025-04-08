bd3c75508fbd6559ee7b1e6f31a708685790d06e
oznurtopcu <topcuoznur121@gmail.com>
2025-04-08 21:09:47 +0300

    added best practice notes

--- src/main/java/com/workintech/s17d2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17d2/rest/DeveloperController.java
@@ -1,48 +1,59 @@
 package com.workintech.s17d2.rest;
 
-import com.workintech.s17d2.model.Developer;
-import com.workintech.s17d2.model.JuniorDeveloper;
-import com.workintech.s17d2.model.MidDeveloper;
-import com.workintech.s17d2.model.SeniorDeveloper;
+import com.workintech.s17d2.dto.DeveloperResponse;
+import com.workintech.s17d2.model.*;
 import com.workintech.s17d2.tax.Taxable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
+@RequestMapping("/developers")   //localhost:8585/workintech/developers/.......
 public class DeveloperController {
 
+    //normalde bu mapler private, teste lazım olduğu için public yaptık
     public Map<Integer, Developer> developers;
     private Taxable taxable;
 
-    @PostConstruct
-    public void init() {
-        this.developers = new HashMap<>();
-    }
 
     @Autowired
     public  DeveloperController(Taxable taxable) {
         this.taxable = taxable;
     }
 
-    @GetMapping("/developers")
+    //uygulama ayağa kalkarken yapılması istenenler burada yapılıyor
+    @PostConstruct
+    public void init() {
+        this.developers = new HashMap<>();
+        //this.developers.put(1, new JuniorDeveloper(1, "öznur", 1000d));
+    }
+
+    @GetMapping
     public List<Developer> getDevelopers() {
+        //developers.values().stream().toList();
         return new ArrayList<>(developers.values());
     }
 
-    @GetMapping("/developers/{id}")
+    @GetMapping("/{id}")
     public Developer getDeveloperById(@PathVariable("id") int id) {
         return developers.get(id);
     }
 
-    @PostMapping("/developers")
+//    @GetMapping("/{id}")
+//    public DeveloperResponse getById(@PathVariable("id") int id) {
+//        Developer found =  developers.get(id);
+//        if(found==null) {
+//            return new DeveloperResponse(null, HttpStatus.NOT_FOUND.value(), id + " ile kayıt bulunamadı.");
+//        }
+//        return new DeveloperResponse(found,HttpStatus.OK.value(), id + " ile kayıt bulundu");
+//    }
+
+    @PostMapping
     public ResponseEntity<Developer> addDeveloper(@RequestBody Developer developer) {
         Developer newDev=null;
         switch (developer.getExperience()) {
@@ -61,15 +72,40 @@ public class DeveloperController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newDev);
     }
 
-    @PutMapping("/developers/{id}")
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public DeveloperResponse save(@RequestBody Developer developer) {
+//        Developer createdDeveloper = DeveloperFactory.createDeveloper(developer,taxable);
+//        if(Objects.nonNull(createdDeveloper)) {
+//            developers.put(createdDeveloper.getId(), createdDeveloper);
+//        }
+//        return new DeveloperResponse(createdDeveloper, HttpStatus.CREATED.value(),"create işlemi başarılı");
+//    }
+
+    @PutMapping("/{id}")
     public Developer updateDeveloper(@PathVariable("id") int id, @RequestBody Developer newDeveloper) {
         developers.replace(id, newDeveloper);
         return developers.get(id);
     }
 
-    @DeleteMapping("/developers/{id}")
+//    @PutMapping("/{id}")
+//    public DeveloperResponse update(@PathVariable("id") int id, @RequestBody Developer newDeveloper) {
+//        newDeveloper.setId(id);
+//        Developer newDev = DeveloperFactory.createDeveloper(newDeveloper, taxable);
+//        this.developers.put(id, newDev);
+//        return new DeveloperResponse(newDev, HttpStatus.OK.value(), "update başarılı");
+//    }
+
+    @DeleteMapping("/{id}")
     public void deleteDeveloper(@PathVariable("id") int id) {
         developers.remove(id);
     }
+
+//    @DeleteMapping("/{id}")
+//    public DeveloperResponse delete(@PathVariable("id") int id) {
+//        Developer removedDev = this.developers.get(id);
+//        this.developers.remove(id);
+//        return new DeveloperResponse(removedDev, HttpStatus.NO_CONTENT.value(), "silme başarılı" );
+//    }
 
 }
--- src/main/java/com/workintech/s17d2/tax/DeveloperTax.java
+++ src/main/java/com/workintech/s17d2/tax/DeveloperTax.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DeveloperTax implements Taxable{
+
+    //enum veya bu şekilde constant bırakabiliriz(static final)
+    //    public static final double SIMPLE_TAX_RATE=15d;
+    //    public static final double MIDDLE_TAX_RATE=25d;
+    //    public static final double UPPER_TAX_RATE=35d;
+
     @Override
     public Double getSimpleTaxRate() {
         return 15d;
