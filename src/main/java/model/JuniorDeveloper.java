482e9e1bf8f65e0ea36cbc700bb52e860a4b51c7
oznurtopcu <topcuoznur121@gmail.com>
2025-04-08 14:23:11 +0300

    change id type

--- src/main/java/model/JuniorDeveloper.java
+++ src/main/java/model/JuniorDeveloper.java
@@ -1,7 +1,7 @@
 package model;
 
 public class JuniorDeveloper extends Developer{
-    public JuniorDeveloper(long id, String name, double salary, Experience experience) {
-        super(id, name, salary, experience);
+    public JuniorDeveloper(int id, String name, double salary) {
+        super(id, name, salary, Experience.JUNIOR);
     }
 }
